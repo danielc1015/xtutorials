51a0046ff075d3ca293ba443d44145284baddbe0
Daniel <danielc1015@gmail.com>
2019-08-26 18:23:31 -0400

    stretched link in cards

--- assets/main.js
+++ assets/main.js
@@ -7,17 +7,20 @@ import { videos } from "./data.js";
     var content = '';
 
     videos.forEach((video) => {
+        
         content += '<div class="col-md-3 mb-3">';
         content += '<div class="card">';
         content += '<div class="embed-responsive embed-responsive-16by9">';
         content += '<img src="https://img.youtube.com/vi/' + video.id + '/maxresdefault.jpg" alt="" class="embed-responsive-item">';
         content += '</div>';
         content += '<div class="card-body">';
-        content += '<h4 class="card-title">' + video.title + '</h4>';
+        content += '<h5 class="card-title">' + video.title + '</h5>';
         content += '<p class="card-text">' + video.description + '</p>';
+        content += '<a href="video.html?id=' + video.id + '" class="stretched-link"></a>';
         content += '</div>';
         content += '</div>';
         content += '</div>';
+        
     });
 
     section.innerHTML = content;
