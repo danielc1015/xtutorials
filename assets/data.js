492d7d1680a7c68c9897693575696597b963a1ee
Daniel <danielc1015@gmail.com>
2019-08-26 18:03:04 -0400

    Separate data to other module

---
+++ assets/data.js
@@ -0,0 +1,29 @@
+const videos = [
+    { 
+        id: 'biyvpvx9I7E', 
+        title: 'Progress en BT', 
+        description: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.',
+        longDescription: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.'
+    },
+    { 
+        id: 'aFlRn1vEMQI', 
+        title: 'Progress en BT', 
+        description: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.',
+        longDescription: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.'
+    },
+    { 
+        id: 'biyvpvx9I7E', 
+        title: 'Progress en BT', 
+        description: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.',
+        longDescription: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.'
+    },
+    { 
+        id: 'MjB1Nyx6hns', 
+        title: 'Progress en BT', 
+        description: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.',
+        longDescription: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.'
+    },
+
+];
+
+export { videos };
--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,19 @@
-var videos = [
-    { id: 'biyvpvx9I7E', title: 'Progress en BT', description: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.'},
-    { id: 'aFlRn1vEMQI', title: 'Progress en 2', description: 'Jjajaja de progreso utilizando componente de bootstrap. Uso y detalles extra.'},
-    { id: 'MjB1Nyx6hns', title: 'Progress en 3', description: 'Loolras de progreso utilizando componente de bootstrap. Uso y detalles extra.'},
-    { id: 'biyvpvx9I7E', title: 'Progress en yossu', description: 'Barras de progreso utilizando componente de bootstrap. Uso y detalles extra.'},
-];
+// Import videos data
+import { videos } from "./data.js";
 
-/**
- * Insert each video into videos section
- */
+// Insert each video into videos section
 (() => {
     var section = document.getElementById('videos-section'); //$("#videos-section");
     var content = '';
 
     videos.forEach((video) => {
-        content += '<div class="col-md-3 mb-2">';
+        content += '<div class="col-md-3 mb-3">';
         content += '<div class="card">';
         content += '<div class="embed-responsive embed-responsive-16by9">';
         content += '<img src="https://img.youtube.com/vi/' + video.id + '/maxresdefault.jpg" alt="" class="embed-responsive-item">';
         content += '</div>';
         content += '<div class="card-body">';
-        content += '<h5 class="card-title">' + video.title + '</h5>';
+        content += '<h4 class="card-title">' + video.title + '</h4>';
         content += '<p class="card-text">' + video.description + '</p>';
         content += '</div>';
         content += '</div>';
